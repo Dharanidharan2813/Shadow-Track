f93a486fff371ccaf335dd22ea32d62007b6a515
Dharanidharan2813 <dharanidharans1328@gmail.com>
2026-03-30 13:04:41 +0530

    Modified Doctype

--- shadow_track/resource_management/doctype/shadow_assignment/shadow_assignment.py
+++ shadow_track/resource_management/doctype/shadow_assignment/shadow_assignment.py
@@ -14,7 +14,23 @@ class ShadowAssignment(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		from shadow_track.resource_management.doctype.evaluation_log.evaluation_log import EvaluationLog
+		from shadow_track.resource_management.doctype.learning_log.learning_log import LearningLog
+
 		amended_from: DF.Link | None
+		employee: DF.Link | None
+		end_date: DF.Date | None
+		evaluation_log: DF.Table[EvaluationLog]
+		evaluation_score: DF.Float
+		final_decision: DF.Literal["Not Ready", "Ready"]
+		learning_log: DF.Table[LearningLog]
+		manager: DF.Link | None
+		mentor: DF.Link | None
+		project: DF.Link | None
+		start_date: DF.Date | None
+		status: DF.Literal[
+			"Draft,", "Pending Approval,", "In Training,", "Evaluation Pending", "Completed", "Extended"
+		]
 	# end: auto-generated types
 
 	pass
--- shadow_track/resource_management/doctype/shodow_assignment/shodow_assignment.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Dharanidharan and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Shodow Assignment", {
-// 	refresh(frm) {
-
-// 	},
-// });
